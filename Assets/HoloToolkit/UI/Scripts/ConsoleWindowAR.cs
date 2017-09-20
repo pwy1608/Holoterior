939b167e2d00191dce62e2bf5007abdf78f408b1
wonyp <pwy1608@naver.com>
2017-09-20 23:18:21 +0900

    Console update.

---
+++ Assets/HoloToolkit/UI/Scripts/ConsoleWindowAR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleWindowAR : MonoBehaviour {
+
+    struct Log
+    {
+        public string mesage;
+        public string stackTrace;
+        public LogType type;
+    }
+
+    private Text text;
+    List<Log> logs = new List<Log>();
+    GameObject consoleTextWindow;
+    GameObject consoleCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
