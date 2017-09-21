cd2339a4c9974f0fcb5070abdb4cc1fb727ff04f
wonyp <pwy1608@naver.com>
2017-09-21 22:01:38 +0900

    added ConsoleWindowsAR, microphone capability update.

--- Assets/HoloToolkit/UI/Scripts/ConsoleWindowAR.cs
+++ Assets/HoloToolkit/UI/Scripts/ConsoleWindowAR.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ConsoleWindowAR : MonoBehaviour {
+public class ConsoleWindowAR : MonoBehaviour
+{
 
     struct Log
     {
-        public string mesage;
+        public string message;
         public string stackTrace;
         public LogType type;
     }
@@ -16,14 +17,150 @@ public class ConsoleWindowAR : MonoBehaviour {
     List<Log> logs = new List<Log>();
     GameObject consoleTextWindow;
     GameObject consoleCanvas;
+    GameObject debugError;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool show;
+    public int clearCount = 23;
+
+    static readonly Dictionary<LogType, Color> logTypeColors = new Dictionary<LogType, Color>()
+    {
+        { LogType.Assert, Color.white },
+        { LogType.Error, Color.red },
+        { LogType.Exception, Color.red },
+        { LogType.Log, Color.white },
+        { LogType.Warning, Color.yellow },
+    };
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+
+        consoleTextWindow = GameObject.Find("ConsoleText");
+        consoleCanvas = GameObject.Find("ConsoleCanvas");
+        text = consoleTextWindow.GetComponent<Text>();
+        show = true;
+
+        Debug.Log("OnEnable");
+    }
+
+    void Update()
+    {
+        if (logs.Count >= clearCount)
+        {
+            ClearConsole();
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            debugError.name = "nothing";
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            RandomMessage();
+        }
+    }
+
+    public void ShowKeyword()
+    {
+        Debug.Log("Show");
+        consoleCanvas.SetActive(true);
+        show = true;
+    }
+
+    public void HideKeyword()
+    {
+        Debug.Log("Hide");
+        consoleCanvas.SetActive(false);
+        show = false;
+    }
+
+    public void ClearConsole()
+    {
+        logs.Clear();
+    }
+
+    void HandleLog(string message, string stackTrace, LogType type)
+    {
+        string nullstring = "";
+        text.text = nullstring;
+        string temp = text.text;
+
+        logs.Add(new Log()
+        {
+            message = message,
+            stackTrace = stackTrace,
+            type = type,
+        });
+
+        for (int i = 0; i < logs.Count; i++)
+        {
+            var log = logs[i];
+            text.color = logTypeColors[log.type];
+            text.text += temp + "\n" + log.message;
+            text.text += "\n" + log.stackTrace;
+        }
+    }
+
+    public void RandomMessage()
+    {
+        string str;
+        int i = Random.Range(1, 20);
+        str = Random.Range(0.0000000000000001f, 9999999999.0f).ToString();
+
+        if (i >= 1 && i <= 5)
+        {
+            HandleLog(str, "somethingIDK", LogType.Exception);
+        }
+
+        if (i > 5 && i < 8)
+        {
+            HandleLog("this is a debug message", "this should be a stacktrace", LogType.Log);
+        }
+
+        if (i >= 8 && i < 12)
+        {
+            HandleLog("this is a Error message", "stackTrace", LogType.Error);
+        }
+
+        if (i >= 12 && i < 15)
+        {
+            HandleLog("this is a Warning message", "stackTrace", LogType.Warning);
+        }
+
+        if (i >= 15 && i < 18)
+        {
+            HandleLog("this is a Exception message", "stackTrace", LogType.Exception);
+        }
+
+        if (i >= 18 && i < 21)
+        {
+            HandleLog("this is a Assertion message", "stackTrace", LogType.Assert);
+        }
+
+    }
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (logs.Count >= clearCount)
+    //    {
+    //        ClearConsole();
+    //    }
+
+    //    if (Input.GetKeyDown("1"))
+    //    {
+    //        debugError.name = "nothing";
+    //    }
+
+    //    if (Input.GetKeyDown("2"))
+    //    {
+    //        RandomMessage();
+    //    }
+    //}
 }
